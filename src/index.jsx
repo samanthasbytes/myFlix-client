cd3999c4cc940e82fb045374f6f857e21264919c
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2024-11-14 00:53:55 -0500

    Initial Redux store setup with movies reducer

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,17 @@ import {createRoot} from 'react-dom/client';
 import Container from 'react-bootstrap/Container';
 import {MainView} from './components/main-view/main-view';
 import './index.scss'; // import statement to indicate that you need to bundle index.scss
+import {store} from './redux/store';
+import {Provider} from 'react-redux';
 
 // main component
 const MyFlixApplication = () => {
   return (
-    <Container>
-      <MainView />
-    </Container>
+    <Provider store={store}>
+      <Container>
+        <MainView />
+      </Container>
+    </Provider>
   );
 };
 
